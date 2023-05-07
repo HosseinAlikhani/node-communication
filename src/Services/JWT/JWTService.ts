ee56b3de0465eaacdb14faecacce9c2e6383b091
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-08 01:06:13 +0330

    udpate: add encodePassPhras method

--- src/Services/JWT/JWTService.ts
+++ src/Services/JWT/JWTService.ts
@@ -22,15 +22,22 @@ export default class JWTService
     private publicCertName?: string;
 
     /**
+     * store cert private name
+     * @var ?string
+     */
+    private privateCertName?: string;
+
+    /**
      * constructor jwt service
      * @param string alg 
      * @param string path 
      */
-    public constructor(alg: string = 'RS256', path?: string, publicCertName?: string)
+    public constructor(alg: string = 'RS256', path?: string, publicCertName?: string, privateCertName?: string)
     {
         this.alg = alg;
         this.path = path ?? `${process.env.PWD}/ssh-key/`;
         this.publicCertName = publicCertName ?? 'public-key.pem';
+        this.privateCertName = privateCertName ?? 'private-key.pem';
     }
 
     /**
@@ -41,5 +48,16 @@ export default class JWTService
     {
         var cert = fs.readFileSync(`${this.path}${this.publicCertName}`);  // get public key
         return jwt.verify(token, cert);
+    }
+
+    /**
+     * encode data with RS256 alg
+     * @param data 
+     * @returns 
+     */
+    public encodePassPhrase(data: object): string
+    {
+        var privateCert = fs.readFileSync(`${this.path}${this.privateCertName}`);
+        return jwt.sign( data, privateCert, { algorithm: 'RS256' });
     }
 }
