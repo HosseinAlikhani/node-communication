c440545d1fcaf87359c71c801205f25ded783ef2
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-08 13:54:06 +0330

    update: add getHeaders closure

--- src/Infrastructure/Request/RequestInterface.ts
+++ src/Infrastructure/Request/RequestInterface.ts
@@ -2,7 +2,15 @@ export default interface RequestInterface
 {
     /**
      * get request object
-     * @return object
+     * @param ?string key
+     * @return object|null
      */
-    getContent(): object
+    getContent(key?: string): object|null;
+
+    /**
+     * get headers
+     * @param string|null key
+     * @return object|null
+     */
+    getHeaders(key?: string): object|null;
 }
