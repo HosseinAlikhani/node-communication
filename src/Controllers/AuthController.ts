6633716bd8ae1034995b98730673033942b85703
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-08 20:55:21 +0330

    update: add checkAuhtenticate method

--- src/Controllers/AuthController.ts
+++ src/Controllers/AuthController.ts
@@ -1,4 +1,19 @@
+import RequestInterface from "../Infrastructure/Request/RequestInterface";
+import ResponseInterface from "../Infrastructure/Response/ResponseInterface";
+
 export default class AuthController
 {
-    
+    /**
+     * check authenticate code
+     * @param RequestInterface req 
+     * @param ResponseInterface res 
+     * @returns 
+     */
+    public static checkAuhtenticate(req: RequestInterface, res: ResponseInterface)
+    {
+        return res.responseJson({
+            data: req.auth()
+        });
+    }
+
 }
