e586b848ab87ae9763929493fe805ca43f17fe18
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-08 20:53:31 +0330

    update: update AuthenticateMiddleware middleware

--- src/Middlewares/AuthenticateMiddleware.ts
+++ src/Middlewares/AuthenticateMiddleware.ts
@@ -1,5 +1,7 @@
 import MiddlewareInterface from "../Infrastructure/Contracts/MiddlewareInterface";
 import RequestInterface from "../Infrastructure/Request/RequestInterface";
+import ResponseInterface from "../Infrastructure/Response/ResponseInterface";
+import JWTService from "../Services/JWT/JWTService";
 
 export default class AuthenticateMiddleware implements MiddlewareInterface
 {
@@ -9,9 +11,32 @@ export default class AuthenticateMiddleware implements MiddlewareInterface
      * @param res 
      * @param next 
      */
-    public handle(req: RequestInterface, res, next)
+    public handle(req: RequestInterface, res: ResponseInterface, next)
     {
         let authorizationToken = req.getHeaders('authorization');
-        return next();
+        if(! authorizationToken){
+            return res.responseJson({
+                status: 401,
+                message: 'need authorization'
+            });
+        }
+
+        try{
+            let data = (new JWTService).decodePassPhrase(authorizationToken);
+            if(! data.uuid ){
+                return res.responseJson({
+                    status: 401,
+                    message: 'authorization code not valid ...!'
+                });
+            }
+
+            req.setAuthUser(data);
+            return next();
+        }catch(e){
+            return res.responseJson({
+                status: 401,
+                message: 'authorization code not valid ..!'
+            });
+        }
     }
 }
