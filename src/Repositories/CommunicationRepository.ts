5b51a36b1cfde4ec69897183bfbe9f18949ac876
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-10 15:13:17 +0330

    update: add communication private method

--- src/Repositories/CommunicationRepository.ts
+++ src/Repositories/CommunicationRepository.ts
@@ -2,8 +2,22 @@ import Repository from "./Repository";
 
 export default class CommunicationRepository extends Repository
 {
+    protected collection = 'communications';
+
+    /**
+     * connect to communication collection
+     */
+    private async communication()
+    {
+        await this.connect();
+        return await this.getConnection().collection(this.collection);
+    }
+
+    /**
+     * find all communications
+     */
     public async find()
     {
-        //
+        return (await this.communication()).find().toArray();
     }
 }
