5aed41d89148c3015489b7ae6a815c6feb211fdb
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-13 15:10:50 +0330

    update: add initialize method

--- src/Http/Services/CommunicationService.ts
+++ src/Http/Services/CommunicationService.ts
@@ -1,4 +1,19 @@
+import PostCommunicationRequest from "../../Requests/PostCommunicationRequest";
+
 export default class CommunicationService
 {
+    private static instance: CommunicationService|null;
 
+    /**
+     * initialize communication service
+     * @returns CommunicationService
+     */
+    public static initialize(): CommunicationService
+    {
+        if(! this.instance){
+            this.instance = new this();
+        }
+
+        return this.instance;
+    }
 }
