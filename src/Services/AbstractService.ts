692097b08b74a17cace7a8eabaadac888051c017
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-14 21:20:49 +0330

    update: add isServiceValid method

--- src/Services/AbstractService.ts
+++ src/Services/AbstractService.ts
@@ -7,4 +7,29 @@ export default class AbstractService
     };
 
     protected static instnace;
+
+    public static isServiceValid(_service)
+    {
+        // check if exists in serviceIDs property
+        let service = this.serviceIDs[_service];
+        if(! service){
+            throw new Error(global.trans("ServiceNotValid", {
+                service: _service
+            }));
+        }
+
+        try{
+            // check service is exists or not
+            if(fs.existsSync(`${process.env.PWD}/src/Services/${service}.ts`)){
+                return true;
+            }
+            throw new Error(global.trans("ServiceNotValid", {
+                service: service
+            }));
+        }catch(e){
+            throw new Error(global.trans("ServiceNotValid", {
+                service: service
+            }));
+        }
+    }
 }
