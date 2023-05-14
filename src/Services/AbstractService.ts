92e3e6a8a509aae097a0800f91893dbde27a05a6
HosseinAlikhani <hossein.alikhani1374@gmail.com>
2023-05-14 21:20:31 +0330

    add: add AbstractService class

---
+++ src/Services/AbstractService.ts
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+export default class AbstractService
+{
+    protected static serviceIDs: object = {
+        "11001": "SlackService"
+    };
+
+    protected static instnace;
+}
