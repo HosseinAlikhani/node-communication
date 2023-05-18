68eef4d07db2b9579d9e2f9cb82164fa5bd1a07c
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-18 16:20:31 +0330

    update: add sendHook method

--- src/Services/SlackService.ts
+++ src/Services/SlackService.ts
@@ -1,3 +1,4 @@
+import Http from "../../Infrastructure/Services/Http/Http";
 import AbstractService from "./AbstractService";
 
 export default class SlackService extends AbstractService
@@ -6,8 +7,31 @@ export default class SlackService extends AbstractService
         "00101": "sendHook"
     };
 
-    public sendHook()
+    public async sendHook()
     {
-        //
+        let url = process.env.SLACK_HOOK_URL,
+            data = {
+                text: this.communication.template
+            };
+
+        try {
+            if(! url){
+                throw new Error("SLACK_HOOK_URL is not set ...!");
+            }
+
+            Http.post(url, data)
+            .then( (res) => {
+                this.delivered();
+            })
+            .catch( (res) => {
+                throw new Error(res.response.data);
+            })
+        }catch(error: any){
+            await this.log({
+                message: error.message,
+                status: 500
+            });
+        }
+
     }
 }
